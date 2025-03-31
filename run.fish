307ff3a6ee906d11405b0847527a00ae73b853c8
giuji <giuji@noreply.codeberg.org>
2025-03-31 16:47:32 +0200

    run.fish execute test

--- run.fish
+++ run.fish
@@ -1,4 +1,5 @@
 set --path OUT out/
+set --path LIB lib/
 
 function done-message
     echo (set_color -o green)"done!"(set_color normal)
@@ -19,26 +20,31 @@ end
 function clean
     echo "Cleaning object files and deps..."
     rm -rf $OUT
+    rm -rf $LIB
     done-message
 end
 
 function tests
     echo "Downloading junit 5..."
     # https://junit.org/junit5/docs/current/user-guide/#running-tests-console-launcher
-    wget -q "https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.9.3/junit-platform-console-standalone-1.9.3.jar" -P $OUT
+    wget -q "https://repo1.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/1.9.3/junit-platform-console-standalone-1.9.3.jar" -P $LIB
+    done-message
+    echo "Compiling tests..."
+    javac -d $OUT -cp "$LIB/*:$OUT" test/**/*.java
     done-message
     echo "Executing Tests..."
-    java -cp $OUT/* org.junit.platform.console.ConsoleLauncher -h # only prints version
+    java -cp "$LIB/*:$OUT" org.junit.platform.console.ConsoleLauncher --scan-classpath
     done-message
 end
 
-mkdir -p $OUT
+mkdir -p $OUT $LIB
 
 switch $argv[1]
     case "run"
 	compile
 	run
     case "test"
+        compile
 	tests
     case '*'
 	echo (set_color -o red)"Either 'run' or 'test'"(set_color normal)
