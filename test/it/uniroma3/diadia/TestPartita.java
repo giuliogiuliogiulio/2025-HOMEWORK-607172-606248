a31bfea60ea2a8f328bbfc51a7b05bf7e9498ebc
iacop <iacop@10.25.152.3>
2025-04-02 16:06:48 +0200

    modifiche import inutili delle classi test di borsa e partita

--- test/it/uniroma3/diadia/TestPartita.java
+++ test/it/uniroma3/diadia/TestPartita.java
@@ -2,10 +2,10 @@ package it.uniroma3.diadia;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import it.uniroma3.diadia.Partita;
 
 class TestPartita {
 	
--- test/it/uniroma3/diadia/giocatore/TestBorsa.java
+++ test/it/uniroma3/diadia/giocatore/TestBorsa.java
@@ -3,10 +3,10 @@ package it.uniroma3.diadia.giocatore;
 import static org.junit.jupiter.api.Assertions.*;
 
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import it.uniroma3.diadia.giocatore.Borsa;
 import it.uniroma3.diadia.attrezzi.Attrezzo;
 
 class TestBorsa {
