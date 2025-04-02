094f0a14dab67526e59fd88f2ce25609ba819255
giuji <giuji@noreply.codeberg.org>
2025-04-02 16:14:43 +0200

    fixed test labirinto test import

--- test/it/uniroma3/diadia/TestPartita.java
+++ test/it/uniroma3/diadia/TestPartita.java
@@ -2,9 +2,10 @@ package it.uniroma3.diadia;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.ambienti.Labirinto;
 
 class TestPartita {
 	
