a6a4aa48a605164ded1f6873a9d37d68f7d4f9f1
iacop <iacop@10.25.152.3>
2025-04-02 15:07:55 +0200

    Merge branch 'main' of https://github.com/giuliogiuliogiulio/2025-HOMEWORK-607172-606248.git into main

--- test/it/uniroma3/diadia/ambienti/TestLabirinto.java
+++ test/it/uniroma3/diadia/ambienti/TestLabirinto.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class testLabirinto {
+class TestLabirinto {
 	Labirinto labirinto;
 	Stanza stanzaCorrente;
 	Stanza stanzaVincente;
