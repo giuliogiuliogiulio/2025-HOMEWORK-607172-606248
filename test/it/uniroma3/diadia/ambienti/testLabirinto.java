e2b5d51b37900cce5b57ff86ed5bc6c160107de1
iacop <iacop@10.25.152.3>
2025-04-02 15:03:43 +0200

    aggiunto testLabirinto

---
+++ test/it/uniroma3/diadia/ambienti/testLabirinto.java
@@ -0,0 +1,37 @@
+package it.uniroma3.diadia.ambienti;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class testLabirinto {
+	Labirinto labirinto;
+	Stanza stanzaCorrente;
+	Stanza stanzaVincente;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		labirinto = new Labirinto();
+	}
+
+	@Test
+	void testNuovoLabirinto() {
+		assertEquals(null, this.labirinto.getStanzaVincente());
+	}
+	
+	@Test
+	void testGetStanzaVincente() {
+		this.labirinto.creaStanze();
+		Stanza vincente = this.labirinto.getStanzaVincente();
+		assertEquals("Biblioteca", vincente.getNome());
+	}
+	
+	@Test
+	void testSetStanzaCorrente() {
+		Stanza corrente = new Stanza("corrente");
+		this.labirinto.setStanzaCorrente(corrente);
+		assertEquals(corrente, this.labirinto.getStanzaCorrente());
+	}
+
+}
