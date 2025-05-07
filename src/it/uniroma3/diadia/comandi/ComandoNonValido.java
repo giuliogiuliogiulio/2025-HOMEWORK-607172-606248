cbf2b029341bca6a3715cbcfeb2caab1a48d9b5c
giuji <giuji@noreply.codeberg.org>
2025-05-07 15:55:30 +0200

    added ComandoNNValido

--- src/it/uniroma3/diadia/comandi/ComandoNonValido.java
+++ src/it/uniroma3/diadia/comandi/ComandoNonValido.java
@@ -6,8 +6,7 @@ public class ComandoNonValido implements Comando {
 
 	@Override
 	public void esegui(Partita partita) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Comando non valido!!");
 	}
 
 	@Override
