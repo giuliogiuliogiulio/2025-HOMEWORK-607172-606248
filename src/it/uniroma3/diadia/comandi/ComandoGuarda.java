cc8749b3d8a62eca9ac7c65175a04b4dd26fb3ff
iacop <iacop@10.21.251.121>
2025-05-07 15:54:57 +0200

    added ComandoGuarda

--- src/it/uniroma3/diadia/comandi/ComandoGuarda.java
+++ src/it/uniroma3/diadia/comandi/ComandoGuarda.java
@@ -1,13 +1,11 @@
 package it.uniroma3.diadia.comandi;
-
 import it.uniroma3.diadia.Partita;
 
 public class ComandoGuarda implements Comando {
 
 	@Override
 	public void esegui(Partita partita) {
-		// TODO Auto-generated method stub
-
+		System.out.println(partita);
 	}
 
 	@Override
