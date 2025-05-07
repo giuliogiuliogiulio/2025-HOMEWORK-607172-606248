4ef2edfe26cb55a79c9bce5230954f4e3da6c254
giuji <giuji@noreply.codeberg.org>
2025-05-07 16:53:44 +0200

    stanzaBloccata

---
+++ src/it/uniroma3/diadia/ambienti/StanzaBloccata.java
@@ -0,0 +1,28 @@
+package it.uniroma3.diadia.ambienti;
+
+public class StanzaBloccata extends Stanza {
+	
+	String direzioneBloccata;
+	String nomeAttrChiave;
+
+	public StanzaBloccata(String nome, String dirBloccata, String attr) {
+		super(nome);
+		this.direzioneBloccata = dirBloccata;
+		this.nomeAttrChiave = attr;
+	}
+	
+	@Override
+	public Stanza getStanzaAdiacente(String dir) {
+		if (dir.equals(direzioneBloccata) && !this.hasAttrezzo(nomeAttrChiave)) {
+			return this;
+		} else {
+			return super.getStanzaAdiacente(dir);
+		}
+	}
+	
+	@Override
+	public String getDescrizione() {
+		return super.getDescrizione() + '\n' + direzioneBloccata + "richiede attrezzo: " + nomeAttrChiave;
+	}
+
+}
