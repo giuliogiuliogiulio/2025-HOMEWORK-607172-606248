3f8574af0ae92252d9c937da467b1690cf69846e
giuji <giuji@noreply.codeberg.org>
2025-05-07 16:36:56 +0200

    added test for posa

---
+++ test/it/uniroma3/diadia/comandi/TestComandoPrendi.java
@@ -0,0 +1,65 @@
+package it.uniroma3.diadia.comandi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+class TestComandoPrendi {
+	
+	Comando prendi;
+	
+	Partita p;
+	Stanza stanzaVuota, stanzaSingolo, stanzaDue;
+	
+	String attr, attr2;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		attr = "ogg";
+		attr2 = "ogg2";
+		
+		stanzaVuota = new Stanza("vuota");
+		
+		stanzaSingolo = new Stanza("singolo");
+		stanzaSingolo.addAttrezzo(new Attrezzo(attr, 0));
+		
+		stanzaDue = new Stanza("due");
+		stanzaDue.addAttrezzo(new Attrezzo(attr, 0));
+		stanzaDue.addAttrezzo(new Attrezzo(attr2, 0));
+		
+		prendi = new ComandoPrendi();
+		p = new Partita();
+	}
+
+	@Test
+	void testPrendiOgg() {
+		p.getLabirinto().setStanzaCorrente(stanzaSingolo);
+		prendi.setParametro(attr);
+		prendi.esegui(p);
+		assertFalse(p.getLabirinto().getStanzaCorrente().hasAttrezzo(attr));
+		assertTrue(p.getGiocatore().getBorsa().hasAttrezzo(attr));
+	}
+	
+	@Test
+	void testPrendiOggVuoto() {
+		p.getLabirinto().setStanzaCorrente(stanzaVuota);
+		prendi.setParametro(attr);
+		prendi.esegui(p);
+		assertFalse(p.getGiocatore().getBorsa().hasAttrezzo(attr));
+	}
+	
+	@Test
+	void testPrendiOggDue() {
+		p.getLabirinto().setStanzaCorrente(stanzaDue);
+		prendi.setParametro(attr);
+		prendi.esegui(p);
+		assertTrue(p.getLabirinto().getStanzaCorrente().hasAttrezzo(attr2));
+		assertFalse(p.getLabirinto().getStanzaCorrente().hasAttrezzo(attr));
+	}
+
+}
