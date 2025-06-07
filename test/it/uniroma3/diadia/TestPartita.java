cb3d2cbe9ab25300cc7c3c3224cb9b069247dc27
giuji <giuji@youlittle.monster>
2025-06-07 12:51:43 +0200

    fixed testPartita

--- test/it/uniroma3/diadia/TestPartita.java
+++ test/it/uniroma3/diadia/TestPartita.java
@@ -6,14 +6,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.uniroma3.diadia.ambienti.Labirinto;
+import it.uniroma3.diadia.ambienti.Labirinto.LabirintoBuilder;
+import it.uniroma3.diadia.ambienti.Stanza;
 
 class TestPartita {
 
 	Partita testPartita;
+	
+	Labirinto lab;
+	
+	Stanza iniziale, vittoria;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		this.testPartita = new Partita(null);
+		iniziale = new Stanza("iniziale");
+		vittoria = new Stanza("vittoria");
+		LabirintoBuilder b = Labirinto.newBuilder();
+		lab = b.addStanza(iniziale)
+				.addStanza(vittoria)
+				.addStanzaIniziale(iniziale.getNome())
+				.addStanzaVincente(vittoria.getNome())
+				.getLabirinto();
+		this.testPartita = new Partita(lab);
 	}
 
 	@Test
@@ -34,9 +48,28 @@ class TestPartita {
 
 	@Test
 	void testVinta() {
-		Labirinto lab = this.testPartita.getLabirinto();
 		lab.setStanzaCorrente(lab.getStanzaVincente());
 		assertTrue(this.testPartita.vinta());
+	}
+	
+	@Test 
+	void testLabirinto() {
+		assertEquals(this.testPartita.getLabirinto(), this.lab);
+	}
+	
+	@Test
+	void testStanzaCorrente() {
+		Stanza s = new Stanza("s");
+		lab.setStanzaCorrente(s);
+		assertEquals(lab.getStanzaCorrente(), s);
+	}
+	
+	@Test
+	void setLabirinto() {
+		assertNotNull(this.testPartita.getLabirinto());
+		Labirinto l = null;
+		testPartita.setLabirinto(l);
+		assertNull(this.testPartita.getLabirinto());
 	}
 
 }
